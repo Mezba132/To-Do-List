da2e27f94827943db02183c4cdc8fd0c24f4a66f
unknown <csmezba@gmail.com>
2020-09-26 21:49:25 +0600

    Update Date Formate

--- Controller/index.js
+++ Controller/index.js
@@ -9,7 +9,12 @@ var items = ["Wash Cloths","Make Coffe"];
 
 app.get("/", (req, res) => {
  let today = new Date();
- let day = today.toDateString();
+ let options = {
+  weekday : "long",
+  month   :  "short",
+  day : "numeric"
+ }
+ let day = today.toLocaleDateString("en-US", options);
 
  res.render("list", {tday : day, newitems: items});
 });
