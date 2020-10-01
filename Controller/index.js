5a3cf84b9839e7290828189801d51629a3a9a8be
unknown <csmezba@gmail.com>
2020-10-01 19:25:12 +0600

    creating custom to-do-list using Express Route parameter

--- Controller/index.js
+++ Controller/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 const date = require("../ExtraModules/date");
 const Item = require("../Models/indexSchema").Item();
+const List = require("../Models/indexSchema").List();
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : true}));
@@ -16,7 +18,6 @@ const item2 = new Item({
  name : "Walk For 20 min"
 })
 const defaultItems = [item1,item2];
-
 
 app.get("/", (req, res) => {
  const day = date.getDate();
@@ -38,11 +39,36 @@ app.get("/", (req, res) => {
  })
 });
 
-app.post("/", (req, res) => {
- let itemName = req.body.additem;
- let listTitle = req.body.addbtn;
+app.get("/:customlist", (req, res) => {
+    const customListName = _.capitalize(req.params.customlist);
+    const emptyItems = [];
+    console.log(customListName);
+    List.findOne({name : customListName}, (err, foundlist) => {
+     if(!err)
+     {
+         if(foundlist)
+         {
+             res.render("list", {listTitle : foundlist.name, newitems : foundlist.items, empty:""});
+         }
+         else {
+             const lists = new List({
+                 name : customListName,
+                 items : emptyItems
+             })
+             lists.save();
+             res.redirect("/" + customListName);
+         }
+     }
+    })
+})
 
- let item = new Item({
+
+
+app.post("/", (req, res) => {
+ const itemName = req.body.additem;
+ const listTitle = req.body.addbtn;
+
+ const item = new Item({
   name : itemName
  })
 
@@ -70,7 +96,7 @@ app.post("/", (req, res) => {
 });
 
 app.post("/delete", (req, res) => {
-  let checkedItem = req.body.checkbox;
+  const checkedItem = req.body.checkbox;
   console.log(checkedItem);
   Item.findByIdAndRemove(checkedItem, (err) => {
    if(!err)
--- Models/indexSchema.js
+++ Models/indexSchema.js
@@ -2,10 +2,19 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/todolistsDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
 exports.Item = () => {
-    const dailySchema = {
+    const ItemSchema = {
         name : String
     }
-    const Item  = mongoose.model("Item", dailySchema);
+    const Item  = mongoose.model("Item", ItemSchema);
     return Item;
+}
+
+exports.List = () => {
+    const listSchema = {
+        name : String,
+        items : [{ name : String }]
+    }
+    const List = mongoose.model("List", listSchema);
+    return List;
 }
 
