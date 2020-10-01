96294b32622e9cc87b881f9faa0092683e6ea822
unknown <csmezba@gmail.com>
2020-10-02 01:21:23 +0600

    update

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ const app = express();
 const route = require("./Controller/index");
 app.use(route);
 
-let port = process.env.PORT;
-if(port === null || port === "")
-{
-    port = 3000;
-}
+// let port = process.env.PORT;
+// if(port === null || port === "")
+// {
+//     port = 3000;
+// }
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is Running Successfully !!");
 })
