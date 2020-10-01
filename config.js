d01dd48d56b77cec09d2d39f0a442ef4d78edce5
unknown <csmezba@gmail.com>
2020-10-02 00:23:31 +0600

    Modify

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const route = require("./Controller/index");
 app.use(route);
 
 let port = process.env.PORT;
-if(port == null || port == "")
+if(port === null || port === "")
 {
     port = 3000;
 }
